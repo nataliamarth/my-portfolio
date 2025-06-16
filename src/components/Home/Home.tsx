a21879416ab607cb5e80edf6435ac2810658415c
nataliamarth <98548283+nataliamarth@users.noreply.github.com>
2025-06-16 14:02:34 +0200

    Remove unused import

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,15 +11,6 @@ const Home: React.FC = () => {
              Front-End Developer | Berlin
           </p>
         </div>
-
-        {/* <nav className={styles.rightColumn}>
-          <ul className={styles.navList}>
-            <li><Link to="/" className={styles.navLink}>Home</Link></li>
-            <li><Link to="/about" className={styles.navLink}>About</Link></li>
-            <li><Link to="/projects" className={styles.navLink}>Projects</Link></li>
-            <li><Link to="/contact" className={styles.navLink}>Contact</Link></li>
-          </ul>
-        </nav> */}
       </div>
     </section>
   );
